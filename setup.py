884314fbc4c522f1b3a5e70db2ff082246a68c1d
Ambrose J Carr <mail@ambrosejcarr.com>
2017-09-01 23:31:27 -0400

    updated manifest, setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 from setuptools import setup
 
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Natural Language :: English",
+    "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
 setup(
     name='scsequtil',
+    version='0.0.3',
     description='Utilities for large scale single cell data processing',
     author='Ambrose J. Carr',
     author_email='mail@ambrosejcarr.com',
@@ -15,5 +30,6 @@ setup(
         'jinja2',
         'weasyprint'
     ],
+    classifiers=CLASSIFIERS,
     include_package_data=True
 )
